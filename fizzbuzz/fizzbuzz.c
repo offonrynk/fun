713644975527c5942e2103ed1b26e41e231db64d
Nkrumah <nkrumahoffonry@gmail.com>
2018-11-10 18:39:44 +0100

    fizzbuzz: commit for fizzbuzz code

---
+++ fizzbuzz/fizzbuzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  for (int i = 1; i <= 100; i++) {
+    if ((i % 15) == 0) {
+      printf("FizzBuzz\t\n");
+    }
+
+      else if ((i % 3) == 0) {
+      printf("Fizz\t");
+      }
+
+      else if ((i % 5) == 0) {
+      printf("Buzz\t");
+      }
+
+    else {
+      printf("%d\t", i);
+    }
+  }
+
+  printf ("\n");
+  return 0;
+}
