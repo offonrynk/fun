e954476aeebac3f9571fa61b25dd143519cf3421
offonrynk <nkrumahoffonry@gmail.com>
2019-10-07 18:44:45 +0200

    [repo]: Normalize line endings and minor unittests adjustments

--- student_database_management_system/unittests.cpp
+++ student_database_management_system/unittests.cpp
@@ -2,14 +2,18 @@
 #include "student.h"
 #include <iostream>
 
-int main() {
-  std::cout << "Basic Unit Tests " << "\n";
-  std::cout << "---------------- " << "\n";
+/*TODO: Improve UnitTests
+        Implement User-friendly scenario
+*/
+int main()
+{
+  std::cout << "Basic Unit Tests " << std::endl;
+  std::cout << "---------------- " << std::endl;
 
   Records r;
-  Student* James = new Student("James John");
-  Student* Dave = new Student("David Kidd");
-  Student* Tom = new Student("Tom Heart");
+  Student *James = new Student("James John");
+  Student *Dave = new Student("David Kidd");
+  Student *Tom = new Student("Tom Heart");
 
   James->addMajor("Electrical Engineering");
   James->addCourses("Math101, Phy101, Chem101");
@@ -23,28 +27,24 @@ int main() {
   Tom->addCourses("Math101, Eco101, Bus101");
   r.addRecord(Tom);
 
-  std::vector<Student* > rec = r.listRecords();
-  for (const auto& it : rec) {
+  std::vector<Student *> rec = r.listRecords();
+  for (const auto &it : rec) {
     std::cout << it->getName() << std::endl;
     std::cout << it->getRegNum() << std::endl;
     std::cout << it->majorProgram() << std::endl;
     std::cout << it->getCourses() << '\n' << '\n';
   }
- 
-  r.deleteRecords(James);
-  std::cout << "Check if the Record for James is Deleted " 
-            << '\n' << std::endl;
 
-  std::vector<Student* > recEdited = r.listRecords();
-  for (const auto& it : recEdited) {
+  r.deleteRecords(James);
+  std::cout << "Check if the Record for James is Deleted " << '\n' << std::endl;
+
+  std::vector<Student *> recEdited = r.listRecords();
+  for (const auto &it : recEdited) {
     std::cout << it->getName() << std::endl;
     std::cout << it->getRegNum() << std::endl;
     std::cout << it->majorProgram() << std::endl;
     std::cout << it->getCourses() << '\n' << '\n';
   }
-  /*TODO: Improve UnitTests
-          Implement User-friendly scenario
-  */
-//  system("pause");
+
   return 0;
 }
